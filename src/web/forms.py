166069410fb84c0663dbaf5c403d39a337a7d2dd
Victoria Lebedeva <43277516+VictoriaLebedeva@users.noreply.github.com>
2022-09-12 12:46:53 +0300

    delete form.py

--- src/web/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class URLForm(FlaskForm):
-    """URL submit form."""
-    name = StringField('Name')
-    submit = SubmitField('Submit')
